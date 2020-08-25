f0606c941e3fc7b6fc2611df563c387db209ea18
josephtmartin <goldglovejo13@gmail.com>
2020-08-25 16:22:38 -0500

    displayed the cards and progress bar addding functionality later

--- javascripts/components/makeDino.js
+++ javascripts/components/makeDino.js
@@ -1,5 +1,5 @@
 const makeDinoCards = (array) => {
-  array.forEach((item) => {
+  array.forEach((item, index) => {
     $('#kennelCards').addClass('kennel-cards');
     $('#kennelCards').append(
       `<div class="card" style="width: 18rem;">
@@ -9,15 +9,16 @@ const makeDinoCards = (array) => {
                     <div class="progress">
                         <div class="progress-bar" style="width: ${item.health}%" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <button class="btn btn-success">Feed</button>
-                    <button class="btn btn-success">Pet</button>
-                    <button class="btn btn-info">Info</button>
-                    <button class="btn btn-danger">Delete</button>
+                    <button class="btn btn-success" id="feed-${index}">Feed</button>
+                    <button class="btn btn-success" id="pet-${index}">Pet</button>
+                    <button class="btn btn-info" id="info-${index}">Info</button>
+                    <button class="btn btn-danger" id="delete-${index}">Delete</button>
                 </div>
             </div>
         `
     );
   });
 };
+
 
 export { makeDinoCards };
