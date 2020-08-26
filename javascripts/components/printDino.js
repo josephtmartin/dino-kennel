2db14498b949170782ef7a7e2bf4db3c9ee8b1f2
josephtmartin <goldglovejo13@gmail.com>
2020-08-25 19:24:47 -0500

    added delete button function

--- javascripts/components/printDino.js
+++ javascripts/components/printDino.js
@@ -1,5 +1,6 @@
 import { makeDino } from './dinoCard.js';
-import { feedPetDino } from './feedPet.js'
+import { feedDino } from './feedDino.js';
+import { petDino } from './petDino.js';
 import { deleteDino } from './deleteDino.js';
 
 const printDinoCards = (array) => {
@@ -12,7 +13,8 @@ const printDinoCards = (array) => {
     } else if (item.health === 0) {
       $('#graveyardCards').append(makeDino(item, index));
     }
-    feedPetDino(index, item, array);
+    feedDino(index, item, array)
+    petDino(index, item, array)
     deleteDino(array, index)
   });
 };
